27a3d564c4eeabafae2caaa1a7b95e53a70b4de7
jzunion5000 <110791919+jzunion5000@users.noreply.github.com>
2022-09-08 18:07:58 -0400

    Add files via upload

---
+++ Foundations.Assessment.BookshelfData/js/bookshelf.js
@@ -0,0 +1,38 @@
+function Bookshelf(books = []) {
+  this.books = books;
+
+  this.addBook = function (book) {
+    this.books.push(book);
+  };
+
+  this.removeBook = function (book) {
+    // Find a book with the same title
+    const idx = this.books.map((b) => b.title).indexOf(book.title);
+    if (idx !== -1) {
+      this.books.splice(idx, 1);
+      return book;
+    } else {
+      return null;
+    }
+  };
+
+  /* CHANGE RENDER! THIS IS A TEMPLATE */
+  this.render = function (index) {
+    const ul = document.createElement("ul");
+    ul.id = `books{index}`
+    const bookRender = this.books.map((c, i) => c.render())
+    ul.replaceChildren(...bookRender);
+    return ul;
+  };
+}
+
+const button = document.querySelector(".button")
+button.addEventListener("click", () => {
+  let author = document.querySelector(".text-author").value;
+  let title = document.querySelector(".text-title").value;
+  let topic = document.querySelector(".text-topic").value;
+
+  const results = books.filter(words => words.includes(title))
+  return results;
+  console.log(results);
+})
